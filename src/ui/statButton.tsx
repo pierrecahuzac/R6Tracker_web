1aa46abf2d5e1460ea7d66dffcd413bf80193094
Thaliios <cahuzac.p@gmail.com>
2025-10-17 23:22:26 +0200

    fix button size on round

--- src/ui/statButton.tsx
+++ src/ui/statButton.tsx
@@ -1,22 +1,13 @@
 import type { StatButtonProps } from "../type/statButton";
 
 const StatButton = ({ title, value, stat, setRound, round }: StatButtonProps) => {
-
-
     const isSelected = round[stat] === value;
-    
-
     const typeClass = `button__${typeof value}`;
-
     const statusClass = isSelected ? "button__selected" : "button__unselected";
-
     return (
         <button
-           
             className={`${statusClass} ${typeClass}`}
-            
             onClick={() => {
-               
                 setRound({
                     ...round,
                     [stat]: value
